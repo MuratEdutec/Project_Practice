0f52defee94277a03d9b701c80c7138a6ada45ed
MuratO.Edutec <muratedutec@gmail.com>
2022-11-08 17:01:00 +0100

    devam

--- src/pp01_ATM.java
+++ src/pp01_ATM.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class pp01_ATM {
     /*ATM
     Kullanicidan giriş için kart numarasi ve şifresini isteyin, eger herhangi birini yanlis girerse tekrar isteyin.
@@ -12,6 +14,77 @@ public class pp01_ATM {
     Sifre değiştirme işleminde mevcut şifreyi teyit ettikten sonra, sifre değişiklik işlemini yapmali,
     */
 
+    final String kartNo="1234567890123456";
+    static Scanner scan= new Scanner(System.in);
+    static String sifre="1234";
+    static double bakiye=20000;
+
+
+
+
+    public static void main(String[] args) {
+    giris();
+
+
+    }
+
+    private static void giris() {
+        System.out.println("Kart Numarasi Giriniz ");
+        String kkartNo= scan.nextLine();
+        System.out.println("Sife Giriniz");
+        String kSifre= scan.next();
+        kkartNo=kkartNo.replaceAll("\\s","");
+
+        if (kSifre.equals(sifre) && kkartNo.equals(kkartNo)){
+            System.out.println("ok");
+            menu();
+        }
+    }
+
+    private static void menu() {
+        System.out.println("****** JAVABANKA HISGELDINIZ******\n" +
+                "Yapmak Istediginiz Islemi Seciniz\n" +
+                "1. Bakiye Sorgulama\n" +
+                "2. Para Yatirma\n" +
+                "3. Para Cekme\n" +
+                "4. Para Gonderme\n" +
+                "5. Sifre Degistirme\n" +
+                "6. CIKIS");
+
+        int secim = scan.nextInt();
+        switch (secim){
+            case 1:{ bakiyeSorgula();}
+            case 2:{
+                System.out.println("Yatirilacak Miktari Giriniz");
+                double miktar=scan.nextDouble();
+                paraYatirma(miktar);  }
+            case 3:{
+                System.out.println("Cekilecek Miktari Giriniz :");
+                double miktar=scan.nextDouble();
+                paracekme();
+            }
+            case 4:{        }
+            case 5:{        }
+            case 6:{        }
+
+     }
+
 
 
 }
+
+    private static void paracekme() {
+    }
+
+    private static void paraYatirma(double miktar) {
+        bakiye+=miktar;
+        bakiyeSorgula();
+        menu();
+
+    }
+
+    private static void bakiyeSorgula() {
+        System.out.println("Bakiyeniz: " + bakiye);
+
+    }
+    }
